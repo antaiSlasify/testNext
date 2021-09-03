ea5afe158efc8f0f962974e76c6656981676df7b
Antai Li <antai@slasify.com>
2021-09-03 11:19:10 +0800

    Test change to nf_lang cookie

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,4 +1,5 @@
 export const setLocaleCookie = (content: string) => {
   if (typeof document === 'undefined') return;
-  document.cookie = `NEXT_LOCALE=${content}`;
+  // document.cookie = `NEXT_LOCALE=${content}`;
+  document.cookie = `nf_lang=${content}`;
 };
